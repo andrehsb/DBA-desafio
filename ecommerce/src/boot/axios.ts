749795c378fb6f966c31c8a9608f63707a4416b2
andrehsb <andrehsb06@gmail.com>
2026-02-25 11:20:07 -0300

    feat:Inicializa quasar project

---
+++ ecommerce/src/boot/axios.ts
@@ -0,0 +1,18 @@
+import { defineBoot } from '#q-app/wrappers';
+import axios, { type AxiosInstance } from 'axios';
+
+declare module 'vue' {
+  interface ComponentCustomProperties {
+    $axios: AxiosInstance;
+    $api: AxiosInstance;
+  }
+}
+
+const api = axios.create({ baseURL: 'http://167.249.210.20:3000' });
+
+export default defineBoot(({ app }) => {
+  app.config.globalProperties.$axios = axios;
+  app.config.globalProperties.$api = api;
+});
+
+export { api };
---
+++ ecommerce/src/components/models.ts
@@ -0,0 +1,12 @@
+export interface Product {
+  id?: number;
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  category: string;
+}
+
+export interface AuthResponse {
+  token: string;
+}
